2cdad814f57774aa9333ab0555d8d7ed51bd51de
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-27 00:20:21 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -6,40 +6,98 @@ class Owner
     @pets = pets
   end
   
-  def 
+  require 'pry'
+
+class Owner
+  attr_accessor :name, :pets
+  attr_reader :species
   
-  def 
-Owner
-  Class methods
-    ::all returns all instances of Owner that have been created (FAILED - 1)
-    ::count returns the number of owners that have been created (FAILED - 2)
-    ::reset_all can reset the owners that have been created (FAILED - 3)
-  Instance methods
-    #species
-      initializes with a species (FAILED - 4)
-      can't change its species (FAILED - 5)
-    #say_species
-      can say its species (FAILED - 6)
-    #name
-      can have a name (FAILED - 7)
-    #pets
-      is initialized with a pets attribute as a hash with 3 keys (FAILED - 8)
-    #buy_fish
-      can buy a fish that is an instance of the Fish class (FAILED - 9)
-      knows about its fishes (FAILED - 10)
-    #buy_cat
-      can buy a cat that is an instance of the Cat class (FAILED - 11)
-      knows about its cats (FAILED - 12)
-    #buy_dog
-      can buy a dog that is an instance of the Dog class (FAILED - 13)
-      knows about its dogs (FAILED - 14)
-    #walk_dogs
-      walks the dogs which makes the dogs' moods happy (FAILED - 15)
-    #play_with_cats
-      plays with the cats which makes the cats moods happy (FAILED - 16)
-    #feed_fish
-      feeds the fishes which makes the fishes' moods happy (FAILED - 17)
-    #sell_pets
-      can sell all its pets, which make them nervous (FAILED - 18)
-    #list_pets
-      can list off its pets (FAILED - 19)
+    @@all= []
+    #why do I use it here?
+
+  # Instance Methods #
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def say_species
+    return "I am a #{@species}."
+  end
+
+  # Pets #
+
+  def buy_fish(name_of_fish)
+    @pets[:fishes] << Fish.new(name_of_fish)
+  end
+
+  def buy_dog(name_of_dog)
+    @pets[:dogs] << Dog.new(name_of_dog)
+  end
+
+  def buy_cat(name_of_cat)
+    @pets[:cats] << Cat.new(name_of_cat)
+  end
+
+  def walk_dogs
+    @pets.collect do |species, instances|
+      if species == :dogs
+        instances.each do |dog|
+          dog.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def play_with_cats
+    @pets.collect do |species, instances|
+      if species == :cats
+        instances.each do |cat|
+          cat.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def feed_fish
+    @pets.collect do |species, instances|
+      if species == :fishes
+        instances.each do |fish|
+          fish.mood = "happy"
+        end
+      end
+    end
+  end
+
+  def sell_pets
+    @pets.collect do |species, instances|
+      instances.each do |pet|
+        pet.mood = "nervous"
+      end
+      instances.clear
+    end
+  end
+
+  def list_pets
+    num_dogs = @pets[:dogs].size
+    num_cats = @pets[:cats].size
+    num_fish = @pets[:fishes].size
+    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+  end
+
+  # Class Methods #
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+end
