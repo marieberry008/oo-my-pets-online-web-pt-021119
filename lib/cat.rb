d616efa904fc61a1398c9ce994f6dcdebecc2520
Marie Berry (Leugermann) <marie@creativeos.io>
2019-03-27 00:24:05 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  # why am I not using this here? @@all = []
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,11 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  # why am I not using this here? @@all = []
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
